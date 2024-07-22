586e16a987f9de6f00324f8cfc11b142a528f020
ShacksVS <inode16@gmail.com>
2024-07-22 22:18:21 +0300

    implemented a BTC rate update logic

---
+++ ExpenseTracker/Managers/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  ExpenseTracker
+//
+//  Created by Viktor Sovyak on 7/22/24.
+//
+
+import Foundation
---
+++ ExpenseTracker/Models/BitcoinRate.swift
@@ -0,0 +1,8 @@
+//
+//  BitcoinRate.swift
+//  ExpenseTracker
+//
+//  Created by Viktor Sovyak on 7/22/24.
+//
+
+import Foundation

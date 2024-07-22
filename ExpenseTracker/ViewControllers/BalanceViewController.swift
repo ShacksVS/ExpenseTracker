05198622daae0eb14f78b8c32e40270ce607d8a0
ShacksVS <inode16@gmail.com>
2024-07-22 16:40:59 +0300

    added stack view and Add transaction button mocks

--- ExpenseTracker/ViewControllers/BalanceViewController.swift
+++ ExpenseTracker/ViewControllers/BalanceViewController.swift
@@ -10,28 +10,110 @@ import UIKit
 class BalanceController: UIViewController {
     
     let mocks = ["1", "2", "3"]
+    let mockBalance = "12332.12"
     
-    private let tableView = UITableView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        setupTableView()
-    }
-    
-    private func setupTableView() {
+    lazy private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(TransactionCell.self, forCellReuseIdentifier: "TransactionCell")
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    lazy private var titleLableView: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "BTC: \(mockBalance)"
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
+    
+    lazy private var addBalanceButtonView: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        var config = UIButton.Configuration.filled()
+        config.buttonSize = .small
+        config.cornerStyle = .medium
+        config.image = UIImage(systemName: "arrow.up")
+        config.imagePadding = 5
+        config.imagePlacement = .leading
+        config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 12, weight: .bold)
+        
+        config.title = "Deposit"
+        
+        button.configuration = config
+        button.addTarget(self, action: #selector(addBalanceButtonPressed), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy private var addTransactionButtonView: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        var config = UIButton.Configuration.bordered()
+        config.buttonSize = .small
+
+        config.cornerStyle = .medium
+        config.image = UIImage(systemName: "plus")
+        config.imagePadding = 5
+        config.imagePlacement = .leading
+        config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 12, weight: .bold)
+        
+        config.title = "Add transaction"
+        
+        button.configuration = config
+        button.addTarget(self, action: #selector(addTransactionButtonPressed), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy private var titleStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLableView, addBalanceButtonView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .trailing
+//        stackView.spacing = 20
+        return stackView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        
+        view.addSubview(titleStackView)
+        view.addSubview(addTransactionButtonView)
         view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            titleStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -24),
+            titleStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            titleStackView.heightAnchor.constraint(equalToConstant: 40),
+            
+            addTransactionButtonView.topAnchor.constraint(equalTo: titleStackView.bottomAnchor, constant: 10),
+            addTransactionButtonView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            addTransactionButtonView.widthAnchor.constraint(equalToConstant: 160),
+            
+            
+            tableView.topAnchor.constraint(equalTo: addTransactionButtonView.bottomAnchor, constant: 10),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+    
+    @objc
+    private func addBalanceButtonPressed(sender: UIButton!){
+       print("add Balance is pressed")
+    }
+    
+    @objc
+    private func addTransactionButtonPressed(sender: UIButton!){
+       print("add Transaction is pressed")
     }
 }
 
